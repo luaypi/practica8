2491b30839a3ff91920d3b3585480fa612506e1f
Luna Ayala <milu_995@hotmail.com>
2024-11-08 22:05:00 +0100

    Ajustar rutas generales de la API

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 // const { checkToken } = require('../utils/middlewares');
 
 router.use('/autores', require('./api/apiAutoresRoutes'));
-// router.use('/posts', require('./api/apiPostsRoutes'));
+router.use('/posts', require('./api/apiPostsRoutes'));
 
 module.exports = router; 
