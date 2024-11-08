dc78db19ff1e9497a43d176fbcc7fd54425e132f
Luna Ayala <milu_995@hotmail.com>
2024-11-08 22:04:30 +0100

    Modificar rutas de API para autores

--- src/routes/api/apiAutoresRoutes.js
+++ src/routes/api/apiAutoresRoutes.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 
 const { getAllAutores, createAutor } = require('../../controllers/autoresController');
-// const { checkAutorId } = require('../../utils/middlewares');
+const { checkAutorId } = require('../../utils/middlewares');
 
 router.get('/', getAllAutores);
-// router.get('/:autorId', checkAutorId, getById);
+router.get('/:autorId', checkAutorId, getById);
+//by lunix
+router. get('/buscar', lookAutoresByName);
+router.get('buscar', lookAutoresByEmail);
+router.get('/:autorId/posts/cantidad', obtainQuantityOfPostsByAuthor);
+router.get('/mas-posts', obtainAutorwithmoreposts);
+router.get('/:autorId/posts/autoresportitulo', obtainTittlePerAutor);
+router.get('/:autorId/posts/autoresporcategoria', obtainCategoryOfAutor);
 
 router.post('/', createAutor);
-// router.put('/:autorId', checkAutorId, updateAutor);
-// router.delete('/:autorId', checkAutorId, deleteCliente);
+router.put('/:autorId', checkAutorId, updateAutor);
+router.delete('/:autorId', checkAutorId, deleteAutor);
 
 module.exports = router;
