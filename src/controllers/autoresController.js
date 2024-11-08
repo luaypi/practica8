2a9188994d14df7d82f48e66b03c730fa94c8251
Luna Ayala <milu_995@hotmail.com>
2024-11-08 22:04:17 +0100

    Actualizar lógica de controladores para autores

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -10,23 +10,23 @@ const getAllAutores = async (req, res, next) => {
 }
 
 
-// const getById = async (req, res, next) => {
-//     const { autorId } = req.params;
-//     try {
-//         const autor = await selectById(autorId);
-//         res.json(autor);
-//     } catch (error) {
-//         next(error);
-//     }
-// }
+const getById = async (req, res, next) => {
+    const { autorId } = req.params;
+    try {
+        const autor = await selectById(autorId);
+        res.json(autor);
+    } catch (error) {
+        next(error);
+    }
+}
 
 //las funciones dentro de controlador son las de modelos
 const createAutor = async (req, res, next) => {
 
     try {
-        // Insertar el nuevo cliente
+        // Insertar el nuevo autor
         const [result] = await insertAutor(req.body);
-        // Recuperar los datos del nuevo cliente
+        // Recuperar los datos del nuevo autor
         const autor = await selectById(result.insertId);
 
         res.json(autor);
@@ -35,32 +35,32 @@ const createAutor = async (req, res, next) => {
     }
 }
 
-// const updateCliente = async (req, res, next) => {
-//     const { clienteId } = req.params;
+const updateAutor = async (req, res, next) => {
+    const { autorId } = req.params;
 
-//     try {
-//         const [result] = await updateClienteById(clienteId, req.body);
-//         const cliente = await selectById(clienteId);
-//         res.json(cliente);
-//     } catch (error) {
-//         next(error);
-//     }
-// }
-
-// const deleteCliente = async (req, res, next) => {
-//     const { clienteId } = req.params;
-
-//     try {
-//         const cliente = await selectById(clienteId);
-//         await deleteById(clienteId);
-//         res.json(cliente);
-//     } catch (error) {
-//         next(error);
-//     }
-// }
-
-module.exports = {
-    getAllAutores, createAutor
+    try {
+        const [result] = await updateAutorById(autorId, req.body);
+        const autor = await selectById(autorIdId);
+        res.json(autor);
+    } catch (error) {
+        next(error);
+    }
 }
 
-// getMayores, createCliente, updateCliente, deleteCliente, getById
+const deleteAutor = async (req, res, next) => {
+    const { autorId } = req.params;
+
+    try {
+        const autor = await selectById(autorId);
+        await deleteById(autorId);
+        res.json(autor);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    getAllAutores, getById, createAutor, updateAutor, deleteAutor
+}
+
+// De ejemplos anteriores: getMayores, createCliente, updateCliente, deleteCliente, getById
