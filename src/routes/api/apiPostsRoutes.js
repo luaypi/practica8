01a13b65d8b14efcdf1f55071e239bc4c681f916
Luna Ayala <milu_995@hotmail.com>
2024-11-09 13:18:31 +0100

    tocando api posts routes

--- src/routes/api/apiPostsRoutes.js
+++ src/routes/api/apiPostsRoutes.js
@@ -9,10 +9,6 @@ router.get('/:autorId', checkPostId, getById);
 router. get('/buscar', lookPostsByName);
 router.get('/buscar', lookPostsByCategoria);
 router.get('/buscar', lookPostsByDateofCreation);
-// router.get('/:autorId/posts/cantidad', obtainQuantityOfPostsByAuthor);
-// router.get('/mas-posts', obtainAutorwithmoreposts);
-// router.get('/:autorId/posts/autoresportitulo', obtainTittlePerAutor);
-// router.get('/:autorId/posts/autoresporcategoria', obtainCategoryOfAutor);
 
 router.post('/', createPost);
 router.put('/:autorId', checkAutorId, updatePost);
