8e2ed8c6b4a688a91f8e7805962ff84d7858c4be
Luna Ayala <milu_995@hotmail.com>
2024-11-09 13:17:46 +0100

    Exportado nuevas rutas de autores

--- src/routes/api/apiAutoresRoutes.js
+++ src/routes/api/apiAutoresRoutes.js
@@ -1,17 +1,11 @@
 const router = require('express').Router();
 
-const { getAllAutores, createAutor } = require('../../controllers/autoresController');
+const { getAllAutores, getById, createAutor, updateAutor, deleteAutor } = require('../../controllers/autoresController');
 const { checkAutorId } = require('../../utils/middlewares');
 
 router.get('/', getAllAutores);
 router.get('/:autorId', checkAutorId, getById);
-//by lunix
-router. get('/buscar', lookAutoresByName);
-router.get('buscar', lookAutoresByEmail);
-router.get('/:autorId/posts/cantidad', obtainQuantityOfPostsByAuthor);
-router.get('/mas-posts', obtainAutorwithmoreposts);
-router.get('/:autorId/posts/autoresportitulo', obtainTittlePerAutor);
-router.get('/:autorId/posts/autoresporcategoria', obtainCategoryOfAutor);
+
 
 router.post('/', createAutor);
 router.put('/:autorId', checkAutorId, updateAutor);
