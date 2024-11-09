37985be760986200351faf2dceb70716dffe6224
Luna Ayala <milu_995@hotmail.com>
2024-11-09 13:19:07 +0100

    cambios api routes revision

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-// const { checkToken } = require('../utils/middlewares');
+const { checkToken } = require('../utils/middlewares');
 
-router.use('/autores', require('./api/apiAutoresRoutes'));
+router.use('/autores', checkToken, require('./api/apiAutoresRoutes'));
 router.use('/posts', require('./api/apiPostsRoutes'));
 
 module.exports = router; 
