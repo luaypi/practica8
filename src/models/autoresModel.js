8e8dec2596a95b37d02ce2bba36aef5ab599db9c
Luna Ayala <milu_995@hotmail.com>
2024-11-09 13:16:20 +0100

    autores model, ajustes

--- src/models/autoresModel.js
+++ src/models/autoresModel.js
@@ -4,28 +4,8 @@ function selectAll() {
     return pool.query('select * from autores;')
 }
 
-async function selectById(autoresId) {
-    const [autores] = await pool.query('select * from autores where id = ?', [autoresId]);
-
-    if (autoresId.length === 0) {
-        return null;
-    }
-
-    return autores[0];
-}
-
-function selectAutorByEmail(email) {
-    if (email.length === 0) {
-        return null;
-    }
-
-    const [result] = pool.query('select * from autores where email = ?', [email]);
-
-    return result[0];
-}
-
-function selectAutorByName({ nombre, email }) {
-    return pool.query('select name, email where nombre like = ?', [nombre, email]);
+async function selectById(autorId) {
+    const [autores] = await pool.query('select * from autores where id = ?', [autorId]);
 
     if (autores.length === 0) {
         return null;
@@ -35,21 +15,22 @@ function selectAutorByName({ nombre, email }) {
 }
 
 function insertAutor ({ nombre, email, imagen }) {
-    return pool.query('insert into autores (nombre, email, imagen) values ( ? , ? , ?)',
+    return pool.query('insert into autores (nombre, email, imagen) values (? , ? , ?)',
         [nombre, email, imagen]
-    )
+    );
 }
 
-function updateAutor ({ nombre, email, imagen }) {
-    return pool.query('update autores set nombre = ?, email = ?, imagen = ?)',
+function updateAutorById (autorId, { nombre, email, imagen }) {
+    return pool.query(
+        'update autores set nombre = ?, email = ?, imagen = ?)',
         [nombre, email, imagen]
-    )
+    );
 }
 
-function deleteAutor ({ nombre, email, imagen }) {
-    return pool.query('delete from autores where id =?', [autoresId])
+function deleteAutorById (autorId ) {
+    return pool.query('delete from autores where id =?', [autorId])
 }
 
 module.exports = {
-    selectAll, selectById, selectAutorByEmail, selectAutorByName, insertAutor, updateAutor, deleteAutor
+    selectAll, selectById, insertAutor, updateAutorById, deleteAutorById
 };
